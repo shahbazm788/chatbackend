f253c2e96b98e4d3e0eb17d05e182c63b5ef1b31
Shahbaz Younus <52800069+shahbazm788@users.noreply.github.com>
2023-12-15 17:25:02 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
-app.listen(5000, () => {
-  console.log("Running on port 5000.");
-});
-const http_server = http.createServer(app);
-// Export the Express API
-// module.exports = app;
+// app.listen(5000, () => {
+//   console.log("Running on port 5000.");
+// });
+// const http_server = http.createServer(app);
+// // Export the Express API
+// // module.exports = app;
 
 
