b527bedf76574f2c1833171cc3767c97742ab10b
Shahbaz Younus <52800069+shahbazm788@users.noreply.github.com>
2023-12-15 17:26:37 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const {Server} = require('socket.io');
 const http = require("http");
-const app = express();
+// const app = express();
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
-// app.listen(5000, () => {
-//   console.log("Running on port 5000.");
-// });
-// const http_server = http.createServer(app);
-// // Export the Express API
-// // module.exports = app;
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
+const http_server = http.createServer(app);
+// Export the Express API
+module.exports = app;
 
 
