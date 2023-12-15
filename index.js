65bd9e8b064a0342da003bcf42e99fb10b7363b3
Shahbaz Younus <52800069+shahbazm788@users.noreply.github.com>
2023-12-15 16:46:29 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,65 @@
-const express = require("express");
+// const express = require("express");
 
+// const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("Express on Vercel");
+// });
+
+// app.listen(5000, () => {
+//   console.log("Running on port 5000.");
+// });
+
+// // Export the Express API
+// module.exports = app;
+
+const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
-});
+const cors = require("cors");
+const {Server} = require('socket.io');
+const http = require("http");
 
-app.listen(5000, () => {
-  console.log("Running on port 5000.");
-});
+const PORT = 5000;
 
-// Export the Express API
-module.exports = app;
+
+app.use(cors())
+const http_server = http.createServer(app);
+
+
+const io = new Server(http_server,{
+    cors:{
+        origin:"https://socket-front-end.vercel.app/",
+        methods : ["GET","POST"]
+    }
+});
+// const io = new Server(http_server,{
+//     cors:{
+//         origin:"http://localhost:3000",
+//         methods : ["GET","POST"]
+//     }
+// });
+io.on("connection",(socket) => {
+    // console.log(socket.id);
+    socket.on("join_room", (data) => {
+        socket.join(data.room)
+      console.log(data)
+        socket.emit("send_room_info",data)
+    });
+    socket.on("send_message", (data) => {
+       
+       socket.to(data.joiendRoom).emit("recive_message",data)
+           
+    });
+    
+
+})
+// http_server.get("/", (req,res) => {
+//     res.send("hi"):
+// });
+
+http_server.listen(PORT,() => {
+    
+    console.log(`express server runing on port ${PORT}`)
+});
+module.exports = http_server;
